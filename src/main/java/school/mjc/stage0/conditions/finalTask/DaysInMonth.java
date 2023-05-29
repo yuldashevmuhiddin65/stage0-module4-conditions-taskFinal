e7301fe1806a2d9bc6c3ca203c42b8c91af5b681
m.yuldashev <yuldashevmuhiddin65@gmail.com>
2023-05-29 10:33:32 +0500

    fixed bugs!

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,22 +2,25 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+
         switch (month) {
             case 1, 3, 5, 7, 8, 10, 12 -> System.out.println(31);
             case 4, 6, 9, 11 -> System.out.println(30);
             case 2 -> {
-                if (year % 4 == 0 && year % 100 == 0 || year % 400 == 0) {
+                if (leap(year))
                     System.out.println(29);
-                } else {
+                else
                     System.out.println(28);
-                }
             }
             default -> System.out.println("invalid date");
         }
     }
 
-    static boolean leap(int year) {
-        return year % 4 == 0 && year % 100 == 0 || year % 400 == 0;
+    private static boolean leap(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
+
 }
+
+
 
